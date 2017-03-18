9b8262f8b2b656f439da649e0618a985dfcc4b87
Lisa <Lisa@lisa-s-mbp.fritz.box>
2017-03-18 15:07:38 +0100

    TS Code prompt

--- Aufgabe_0/Aufgabe_0.js
+++ Aufgabe_0/Aufgabe_0.js
@@ -1,1 +1,12 @@
+/*
+Aufgabe: Nr. 0
+Name: Lisa Würstle
+Matrikel: 254072
+Datum: 18.03.2017
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.*/
+document.addEventListener('DOMContentLoaded', function () {
+    var answer = "Ich wünsche dir einen schönen Tag!";
+    var yourName = prompt("Möchtest du mir deinen Namen verraten?");
+    document.body.innerHTML = answer + yourName;
+});
 //# sourceMappingURL=Aufgabe_0.js.map
---
+++ Aufgabe_0/Aufgabe_0.ts
@@ -0,0 +1,14 @@
+/*
+Aufgabe: Nr. 0
+Name: Lisa Würstle
+Matrikel: 254072
+Datum: 18.03.2017
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.*/
+
+
+document.addEventListener ('DOMContentLoaded', function () {
+    var answer: string = "Ich wünsche dir einen schönen Tag!";
+    var yourName: string = prompt ("Möchtest du mir deinen Namen verraten?");
+    document.body.innerHTML = answer + yourName;
+});
+
