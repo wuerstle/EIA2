4792945d7cdb6b4b7e166e70f8626d7f2270bf30
Lisa <Lisa@lisa-s-mbp.fritz.box>
2017-04-02 12:24:50 +0200

    ts verbessert

--- Aufgabe_0/Aufgabe_0.js
+++ Aufgabe_0/Aufgabe_0.js
@@ -4,7 +4,7 @@ Name: Lisa Würstle
 Matrikel: 254072
 Datum: 18.03.2017
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.*/
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
     var answer = "Ich wünsche dir einen schönen Tag ";
     var yourName = prompt("Möchtest du mir deinen Namen verraten?");
     document.body.innerHTML = answer + yourName + " :)";
--- Aufgabe_0/Aufgabe_0.ts
+++ Aufgabe_0/Aufgabe_0.ts
@@ -6,7 +6,7 @@ Datum: 18.03.2017
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.*/
 
 
-document.addEventListener ('DOMContentLoaded', function () {
+document.addEventListener ("DOMContentLoaded", function (): void {
     var answer: string = "Ich wünsche dir einen schönen Tag ";
     var yourName: string = prompt ("Möchtest du mir deinen Namen verraten?");
     document.body.innerHTML = answer + yourName + " :)";
