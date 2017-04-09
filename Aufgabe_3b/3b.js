e9ab6d2bd2ef92ff76d7a0a978a8686615d0f81a
Lisa <Lisa@lisa-s-mbp.fritz.box>
2017-04-09 17:23:08 +0200

    TS 3b

--- Aufgabe_3b/3b.js
+++ Aufgabe_3b/3b.js
@@ -3,8 +3,8 @@ var Nr3b_MauMau;
     window.onload = function () {
         var cards = ["Karo 7", "Karo 8", "Karo 9", "Karo 10", "Karo Bube", "Karo Dame", "Karo König", "Karo Ass", "Herz 7", "Herz 8", "Herz 9", "Herz 10", "Herz Bube", "Herz Dame", "Herz König", "Herz Ass", "Pik 7", "Pik 8", "Pik 9", "Pik 10", "Pik Bube", "Pik Dame", "Pik König", "Pik Ass", "Kreuz 7", "Kreuz 8", "Kreuz 9", "Kreuz 10", "Kreuz Bube", "Kreuz Dame", "Kreuz König", "Kreuz Ass"];
         var c = cards.length;
-        var handCards;
-        var discard;
+        var handCards = [];
+        var discard = [];
         var h = handCards.length;
         document.getElementById("deckCards").textContent = "Kartenstapel" + " " + c;
         document.getElementById("discard").textContent = "Ablagestapel";
