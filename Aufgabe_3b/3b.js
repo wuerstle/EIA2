b96db48d9ffc4bd9d5369405fff97c82dc26420c
Lisa <Lisa@192.168.0.21>
2017-04-10 19:55:37 +0200

    3b verbessert und verstanden

--- Aufgabe_3b/3b.js
+++ Aufgabe_3b/3b.js
@@ -6,22 +6,54 @@
 //Leider tat ich mir bei dieser Aufgabe sehr schwer. Daher ist der Code auch nicht vollständig. 
 var Nr3b_MauMau;
 (function (Nr3b_MauMau) {
+    var stapel = ["Karo 7", "Karo 8", "Karo 9", "Karo 10", "Karo Bube", "Karo Dame", "Karo König", "Karo Ass", "Herz 7", "Herz 8", "Herz 9", "Herz 10", "Herz Bube", "Herz Dame", "Herz König", "Herz Ass", "Pik 7", "Pik 8", "Pik 9", "Pik 10", "Pik Bube", "Pik Dame", "Pik König", "Pik Ass", "Kreuz 7", "Kreuz 8", "Kreuz 9", "Kreuz 10", "Kreuz Bube", "Kreuz Dame", "Kreuz König", "Kreuz Ass"];
+    var hand = [];
+    var ablage = [];
     window.onload = function () {
-        var cards = ["Karo 7", "Karo 8", "Karo 9", "Karo 10", "Karo Bube", "Karo Dame", "Karo König", "Karo Ass", "Herz 7", "Herz 8", "Herz 9", "Herz 10", "Herz Bube", "Herz Dame", "Herz König", "Herz Ass", "Pik 7", "Pik 8", "Pik 9", "Pik 10", "Pik Bube", "Pik Dame", "Pik König", "Pik Ass", "Kreuz 7", "Kreuz 8", "Kreuz 9", "Kreuz 10", "Kreuz Bube", "Kreuz Dame", "Kreuz König", "Kreuz Ass"];
-        var c = cards.length;
-        var handCards = [];
-        var discard = [];
-        var h = handCards.length;
-        document.getElementById("deckCards").textContent = "Kartenstapel" + " " + c;
-        document.getElementById("discard").textContent = "Ablagestapel";
-        document.getElementById("deckCards").addEventListener("click", function () {
-            if (c > 0 && h < 5) {
-                var random = Math.floor((Math.random() * c) + 0);
-                handCards.push(cards[random]);
-                var div = document.createElement("div");
-                document.getElementById("handCards").appendChild(div);
-            }
-        });
+        createFeld();
     };
+    function createFeld() {
+        var div;
+        for (var i = 0; i < 7; i++) {
+            div = document.createElement("div");
+            switch (i) {
+                case 0:
+                    div.id = "stapel";
+                    div.textContent = "Stapel";
+                    div.addEventListener("click", ziehen);
+                    break;
+                case 6:
+                    div.id = "ablage";
+                    break;
+                default:
+                    div.className = "hand";
+                    div.addEventListener("click", ablegen);
+            }
+            document.body.appendChild(div);
+        }
+    }
+    function ziehen() {
+        if (hand.length < 5) {
+            var n = Math.round(Math.random() * (stapel.length - 1));
+            hand.push(stapel.splice(n, 1)[0]);
+        }
+        update();
+    }
+    function ablegen(event) {
+        var div = event.target;
+        var card = div.textContent;
+        var index = hand.indexOf(card);
+        ablage.push(hand.splice(index, 1)[0]);
+        update();
+    }
+    function update() {
+        var hands = document.getElementsByClassName("hand");
+        for (var i = 0; i < hands.length; i++) {
+            hands[i].textContent = hand[i];
+        }
+        var ablageStapel = document.getElementById("ablage");
+        ablageStapel.textContent = ablage[ablage.length - 1];
+    }
 })(Nr3b_MauMau || (Nr3b_MauMau = {}));
+;
 //# sourceMappingURL=3b.js.map
--- Aufgabe_3b/3b.ts
+++ Aufgabe_3b/3b.ts
@@ -6,26 +6,61 @@
 //Leider tat ich mir bei dieser Aufgabe sehr schwer. Daher ist der Code auch nicht vollständig. 
 
 namespace Nr3b_MauMau {
-window.onload = function(): void {
-    let cards: string [] = ["Karo 7", "Karo 8", "Karo 9", "Karo 10", "Karo Bube", "Karo Dame", "Karo König", "Karo Ass", "Herz 7", "Herz 8", "Herz 9", "Herz 10", "Herz Bube", "Herz Dame", "Herz König", "Herz Ass", "Pik 7", "Pik 8", "Pik 9", "Pik 10", "Pik Bube", "Pik Dame", "Pik König", "Pik Ass", "Kreuz 7", "Kreuz 8", "Kreuz 9", "Kreuz 10", "Kreuz Bube", "Kreuz Dame", "Kreuz König", "Kreuz Ass"]; 
-    let c = cards.length;
-    let handCards: string [] = [];
-    let discard: string [] = [];
-    let h = handCards.length;
+    let stapel: string [] = ["Karo 7", "Karo 8", "Karo 9", "Karo 10", "Karo Bube", "Karo Dame", "Karo König", "Karo Ass", "Herz 7", "Herz 8", "Herz 9", "Herz 10", "Herz Bube", "Herz Dame", "Herz König", "Herz Ass", "Pik 7", "Pik 8", "Pik 9", "Pik 10", "Pik Bube", "Pik Dame", "Pik König", "Pik Ass", "Kreuz 7", "Kreuz 8", "Kreuz 9", "Kreuz 10", "Kreuz Bube", "Kreuz Dame", "Kreuz König", "Kreuz Ass"]; 
+    let hand: string [] = [];
+    let ablage: string [] = [];
 
-    document.getElementById("deckCards").textContent = "Kartenstapel" + " " + c;
-    document.getElementById("discard").textContent = "Ablagestapel";
-    
-    
-    document.getElementById("deckCards").addEventListener("click", function(): void {
-        if (c > 0 && h < 5) {
-            let random: number = Math.floor((Math.random() * c) + 0);
-            handCards.push(cards[random]);
-            let div: HTMLDivElement = document.createElement("div");
-            document.getElementById("handCards").appendChild(div);
-             
+    window.onload = function (): void {
+        createFeld();
+    };
+
+    function createFeld(): void {
+        let div: HTMLDivElement;
+        for (let i: number = 0; i < 7; i++) {
+            div = <HTMLDivElement>document.createElement("div");
+            switch (i) {
+                case 0:
+                    div.id = "stapel";
+                    div.textContent = "Stapel";
+                    div.addEventListener("click", ziehen);
+                    break;
+                case 6:
+                    div.id = "ablage";
+                    break;
+                default:
+                    div.className = "hand";
+                    div.addEventListener("click", ablegen);
+            }
+            document.body.appendChild(div);
         }
-    });  
+    }
+
+    function ziehen(): void {
+        if (hand.length < 5) {
+            let n: number = Math.round(Math.random() * (stapel.length - 1));
+            hand.push(stapel.splice(n, 1)[0]);
+        }
+
+        update();
+    }
+
+    function ablegen(event: MouseEvent): void {
+        let div: HTMLDivElement = <HTMLDivElement>event.target;
+        let card: string = div.textContent;
+        let index: number = hand.indexOf(card);
+        ablage.push(hand.splice(index, 1)[0]);
+
+        update();
+    }
+
+    function update(): void {
+        let hands: NodeListOf<Element> = document.getElementsByClassName("hand");
+        for (let i: number = 0; i < hands.length; i++) {
+            hands[i].textContent = hand[i];
+        }
+
+        let ablageStapel: HTMLDivElement = <HTMLDivElement>document.getElementById("ablage");
+        ablageStapel.textContent = ablage[ablage.length - 1];
+    }
 
 };
-}
