fc034fe00f3a1b55026c0a326e39a99853dcb225
Lisa <Lisa@lisa-s-mbp.fritz.box>
2017-05-14 14:25:41 +0200

    Ts- Datei für Bienen

---
+++ Aufgabe_7/bee.js
@@ -0,0 +1,57 @@
+//Aufgabe: Nr. 7
+//Name: Lisa Würstle
+//Matrikel: 254072
+//Datum: 14.05.2017
+//Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+var L7_Classes;
+(function (L7_Classes) {
+    var Bee = (function () {
+        function Bee(_x, _y, _color, _size) {
+            this.x = _x;
+            this.y = _y;
+            this.color = "hsl(" + Math.random() * 360 + ", 100%, 50%)";
+            this.size = Math.random() * 4 + 2;
+        }
+        Bee.prototype.update = function () {
+            this.move();
+            this.drawBee();
+        };
+        Bee.prototype.drawBee = function () {
+            L7_Classes.crc2.beginPath();
+            L7_Classes.crc2.fillStyle = this.color;
+            L7_Classes.crc2.strokeStyle = "grey";
+            L7_Classes.crc2.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+            L7_Classes.crc2.closePath();
+            L7_Classes.crc2.fill();
+            L7_Classes.crc2.stroke();
+            L7_Classes.crc2.beginPath();
+            L7_Classes.crc2.fillStyle = "#000000";
+            L7_Classes.crc2.arc(this.x + 5, this.y, this.size, 0, 2 * Math.PI);
+            L7_Classes.crc2.closePath();
+            L7_Classes.crc2.fill();
+            L7_Classes.crc2.stroke();
+            L7_Classes.crc2.beginPath();
+            L7_Classes.crc2.fillStyle = this.color;
+            L7_Classes.crc2.arc(this.x + 8, this.y, this.size, 0, 2 * Math.PI);
+            L7_Classes.crc2.closePath();
+            L7_Classes.crc2.fill();
+            L7_Classes.crc2.stroke();
+        };
+        Bee.prototype.move = function () {
+            this.x += Math.random() * 4 - 2.1;
+            this.y += Math.random() * 7 - 3.5;
+            if (this.x < 0) {
+                this.x = L7_Classes.crc2.canvas.width;
+            }
+            if (this.y < 0) {
+                this.y = L7_Classes.crc2.canvas.height;
+            }
+            if (this.y >= L7_Classes.crc2.canvas.height) {
+                this.y = 0;
+            }
+        };
+        return Bee;
+    }());
+    L7_Classes.Bee = Bee;
+})(L7_Classes || (L7_Classes = {}));
+//# sourceMappingURL=bee.js.map
---
+++ Aufgabe_7/bee.ts
@@ -0,0 +1,64 @@
+//Aufgabe: Nr. 7
+//Name: Lisa Würstle
+//Matrikel: 254072
+//Datum: 14.05.2017
+//Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+
+namespace L7_Classes {
+    export class Bee {
+        x: number;
+        y: number;
+        color: string;
+        size: number;
+
+        constructor(_x: number, _y: number, _color: string, _size: number) {
+            this.x = _x;
+            this.y = _y;
+            this.color = "hsl(" + Math.random() * 360 + ", 100%, 50%)";
+            this.size = Math.random() * 4 + 2;
+
+        }
+
+        update(): void {
+            this.move();
+            this.drawBee();
+        }
+
+        drawBee(): void {
+            crc2.beginPath();
+            crc2.fillStyle = this.color;
+            crc2.strokeStyle = "grey";
+            crc2.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+            crc2.closePath();
+            crc2.fill();
+            crc2.stroke();
+            crc2.beginPath();
+            crc2.fillStyle = "#000000";
+            crc2.arc(this.x + 5, this.y, this.size, 0, 2 * Math.PI);
+            crc2.closePath();
+            crc2.fill();
+            crc2.stroke();
+            crc2.beginPath();
+            crc2.fillStyle = this.color;
+            crc2.arc(this.x + 8, this.y, this.size, 0, 2 * Math.PI);
+            crc2.closePath();
+            crc2.fill();
+            crc2.stroke();
+        }
+
+        move(): void {
+            this.x += Math.random() * 4 - 2.1;
+            this.y += Math.random() * 7 - 3.5;
+
+            if (this.x < 0) {
+                this.x = crc2.canvas.width;
+            }
+            if (this.y < 0) {
+                this.y = crc2.canvas.height;
+            }
+            if (this.y >= crc2.canvas.height) {
+                this.y = 0;
+            }
+        }
+    }
+}
