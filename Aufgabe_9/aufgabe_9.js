bfbec6504c58905e7076d56197d7161b567af5ae
Lisa <Lisa@192.168.0.23>
2017-05-31 22:31:48 +0200

    ts-file vereinfacht

--- Aufgabe_9/aufgabe_9.js
+++ Aufgabe_9/aufgabe_9.js
@@ -3,9 +3,13 @@ var A9_FormElements;
     window.addEventListener("load", init);
     //Array für alle Sorten, die in meiner Online-Eisdiele angeboten werden
     var flavours = ["Chocolate", "Vanilla", "Strawberry", "Raspberry", "Bubblegum", "Dark Chocolate", "White Chocolate", "Yogurt", "Kiddy Chocolate", "Banana", "Blueberry"];
+    var inputFlavour = [];
     //Array für alle Behälter, in denen das Eis bestellt werden kann
     var boxes = ["Waffle", "Cup", "Grab Bag", "Wonder Waffle"];
+    var inputBoxes = [];
+    //Array für Lieferoptionen
     var delivery = ["Express", "Standard", "Premium"];
+    var inputDelivery = [];
     //Preis für Eissorte
     var flavourPrice = 1;
     //Preis für Extras
@@ -13,25 +17,21 @@ var A9_FormElements;
     //boxes are free
     //FieldSet in dem alle Inputs angeordnet werden
     var fieldSet;
-    //Array 
-    var inputs = [];
     function init(_event) {
         fieldSet = document.getElementsByTagName("fieldset")[0]; //FieldSet 1
-        createFlavour();
-        fieldSet = document.getElementsByTagName("fieldset")[2]; //FieldSet 3
-        createBoxes();
-        fieldSet = document.getElementsByTagName("fieldset")[4]; //FieldSet 5
-        createDelivery();
+        createCheckbox();
+        fieldSet = document.getElementsByTagName("fieldset")[2, 4]; //FieldSet 3
+        createRadio();
         fieldSet.addEventListener("change", handleChange);
     }
-    function createFlavour() {
+    function createCheckbox() {
         //erstellt pro Eissorte einen Input
         for (var i = 0; i < flavours.length; i++) {
             console.log(flavours[i]);
-            createInput1(flavours[i]);
+            createInputCheckbox(flavours[i]);
         }
     }
-    function createInput1(_flavours) {
+    function createInputCheckbox(_flavours) {
         var label = document.createElement("label");
         var input = document.createElement("input");
         label.innerText = _flavours;
@@ -41,16 +41,16 @@ var A9_FormElements;
         input.value = "flavours";
         input.id = "checkbox1";
         fieldSet.appendChild(label);
-        inputs.push(input);
+        inputFlavour.push(input);
     }
-    function createBoxes() {
+    function createRadio() {
         //erstellt pro Box einen Input
         for (var i = 0; i < boxes.length; i++) {
             console.log(boxes[i]);
-            createInput3(boxes[i]);
+            createInputRadio(boxes[i]);
         }
     }
-    function createInput3(_boxes) {
+    function createInputRadio(_boxes) {
         var label = document.createElement("label");
         var input = document.createElement("input");
         label.innerText = _boxes;
@@ -60,33 +60,16 @@ var A9_FormElements;
         input.value = "boxes";
         input.id = "radiobutton1";
         fieldSet.appendChild(label);
-        inputs.push(input);
-    }
-    function createDelivery() {
-        //erstellt pro Lieferungsmöglichkeit einen Input
-        for (var i = 0; i < delivery.length; i++) {
-            console.log(delivery[i]);
-            createInput5(delivery[i]);
-        }
-    }
-    function createInput5(_delivery) {
-        var label = document.createElement("label");
-        var input = document.createElement("input");
-        label.innerText = _delivery;
-        label.appendChild(input);
-        input.type = "radio"; //Art des Inputs
-        input.name = "RadioDelivery";
-        input.value = "delivery";
-        input.id = "radiobutton2";
-        fieldSet.appendChild(label);
-        inputs.push(input);
+        inputBoxes.push(input);
     }
     function handleChange(_event) {
         var sum = 0;
-        for (var i = 0; i < inputs.length; i++) {
-            sum += parseInt(inputs[i].value);
+        for (var i = 0; i < inputFlavour.length; i++) {
+            if (inputFlavour[i].checked) {
+                sum += 1;
+            }
         }
-        console.log(sum); //gib das Ergenbis auf der Konsole aus
+        console.log(); //gib das Ergenbis auf der Konsole aus
     }
 })(A9_FormElements || (A9_FormElements = {}));
 //# sourceMappingURL=aufgabe_9.js.map
--- Aufgabe_9/aufgabe_9.ts
+++ Aufgabe_9/aufgabe_9.ts
@@ -3,9 +3,15 @@ namespace A9_FormElements {
 
     //Array für alle Sorten, die in meiner Online-Eisdiele angeboten werden
     let flavours: string[] = ["Chocolate", "Vanilla", "Strawberry", "Raspberry", "Bubblegum", "Dark Chocolate", "White Chocolate", "Yogurt", "Kiddy Chocolate", "Banana", "Blueberry"];
+    let inputFlavour: HTMLInputElement[] = [];
+
     //Array für alle Behälter, in denen das Eis bestellt werden kann
     let boxes: string[] = ["Waffle", "Cup", "Grab Bag", "Wonder Waffle"];
+    let inputBoxes: HTMLInputElement[] = [];
+
+    //Array für Lieferoptionen
     let delivery: string[] = ["Express", "Standard", "Premium"];
+    let inputDelivery: HTMLInputElement[] = [];
 
     //Preis für Eissorte
     let flavourPrice: number = 1;
@@ -15,28 +21,24 @@ namespace A9_FormElements {
 
     //FieldSet in dem alle Inputs angeordnet werden
     let fieldSet: HTMLFieldSetElement;
-    //Array 
-    let inputs: HTMLInputElement[] = [];
 
     function init(_event: Event): void {
         fieldSet = document.getElementsByTagName("fieldset")[0]; //FieldSet 1
-        createFlavour();
-        fieldSet = document.getElementsByTagName("fieldset")[2]; //FieldSet 3
-        createBoxes();
-        fieldSet = document.getElementsByTagName("fieldset")[4]; //FieldSet 5
-        createDelivery();
+        createCheckbox();
+        fieldSet = document.getElementsByTagName("fieldset")[2, 4]; //FieldSet 3
+        createRadio();
         fieldSet.addEventListener("change", handleChange);
     }
 
-    function createFlavour(): void {
+    function createCheckbox(): void {
         //erstellt pro Eissorte einen Input
         for (let i: number = 0; i < flavours.length; i++) {
             console.log(flavours[i]);
-            createInput1(flavours[i]);
+            createInputCheckbox(flavours[i]);
         }
     }
 
-    function createInput1(_flavours: string): void {
+    function createInputCheckbox(_flavours: string): void {
         let label: HTMLLabelElement = document.createElement("label");
         let input: HTMLInputElement = document.createElement("input");
         label.innerText = _flavours;
@@ -46,18 +48,18 @@ namespace A9_FormElements {
         input.value = "flavours";
         input.id = "checkbox1";
         fieldSet.appendChild(label);
-        inputs.push(input);
+        inputFlavour.push(input);
     }
 
-    function createBoxes(): void {
+    function createRadio(): void {
         //erstellt pro Box einen Input
         for (let i: number = 0; i < boxes.length; i++) {
             console.log(boxes[i]);
-            createInput3(boxes[i]);
+            createInputRadio(boxes[i]);
         }
     }
 
-    function createInput3(_boxes: string): void {
+    function createInputRadio(_boxes: string): void {
         let label: HTMLLabelElement = document.createElement("label");
         let input: HTMLInputElement = document.createElement("input");
         label.innerText = _boxes;
@@ -67,36 +69,16 @@ namespace A9_FormElements {
         input.value = "boxes";
         input.id = "radiobutton1";
         fieldSet.appendChild(label);
-        inputs.push(input);
-    }
-
-    function createDelivery(): void {
-        //erstellt pro Lieferungsmöglichkeit einen Input
-        for (let i: number = 0; i < delivery.length; i++) {
-            console.log(delivery[i]);
-            createInput5(delivery[i]);
-        }
-    }
-
-    function createInput5(_delivery: string): void {
-        let label: HTMLLabelElement = document.createElement("label");
-        let input: HTMLInputElement = document.createElement("input");
-        label.innerText = _delivery;
-        label.appendChild(input);
-        input.type = "radio"; //Art des Inputs
-        input.name = "RadioDelivery";
-        input.value = "delivery";
-        input.id = "radiobutton2";
-        fieldSet.appendChild(label);
-        inputs.push(input);
+        inputBoxes.push(input);
     }
 
     function handleChange(_event: Event): void {
         let sum: number = 0;
-        for (let i: number = 0; i < inputs.length; i++) {
-            sum += parseInt(inputs[i].value);
+        for (let i: number = 0; i < inputFlavour.length; i++) {
+            if (inputFlavour[i].checked) { 
+            sum += 1; }
         }
-        
-        console.log(sum); //gib das Ergenbis auf der Konsole aus
+
+        console.log(); //gib das Ergenbis auf der Konsole aus
     }
 }
